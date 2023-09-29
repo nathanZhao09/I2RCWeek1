d5cb98363726f8668aa7653fd26b1cb27fbcb040
nathanZhao09 <nathanzhao15@gmail.com>
2023-09-28 21:51:53 -0700

    commented driveTrain code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,8 +6,8 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice; //Lines 9-19 could be importing files for the drive train's code
+import com.ctre.phoenix.motorcontrol.NeutralMode;    //here to
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.networktables.GenericEntry;
@@ -15,51 +15,51 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;   //here
 import frc.robot.Constants;
 
 import com.kauailabs.navx.frc.AHRS;
 
-public class DriveTrain extends SubsystemBase //etc.
+public class DriveTrain extends SubsystemBase //createing a class (public)
 {
   private final WPI_TalonSRX leftDriveTalon;
   private final WPI_TalonSRX rightDriveTalon;
 
   private AHRS navx = new AHRS(SPI.Port.kMXP);
 
-  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain");
-  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry();
+  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain"); //obtaining the shuffle board's tabs (boolean)
+  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry(); //the voltage of the right drive tain
   private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry();
 
   /** Creates a new DriveTrain */
   public DriveTrain() 
   {
-    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
+    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort); //one part of the drive train
     rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
   
-    leftDriveTalon.setNeutralMode(NeutralMode.Coast);
+    leftDriveTalon.setNeutralMode(NeutralMode.Coast); //both set in a neutral mode (not moving)
     rightDriveTalon.setNeutralMode(NeutralMode.Coast);
 
-    leftDriveTalon.setInverted(true);
-    rightDriveTalon.setInverted(false);
+    leftDriveTalon.setInverted(true); //turing on the inverted for the left drive talon 
+    rightDriveTalon.setInverted(false); //turing off the right driver talon
 
-    leftDriveTalon.setSensorPhase(true);
-    rightDriveTalon.setSensorPhase(true);
+    leftDriveTalon.setSensorPhase(true); //turns on the left sensors 
+    rightDriveTalon.setSensorPhase(true); //turns on the right sensors
 
-    leftDriveTalon.configFactoryDefault();
+    leftDriveTalon.configFactoryDefault(); //setting left drive talon to factory settings to run at optimal speeds
     leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-    rightDriveTalon.configFactoryDefault();
+    rightDriveTalon.configFactoryDefault();  //setting right drive train to facotry settings to run at optimal speeds 
     rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 
   }
 
   public void tankDrive(double leftSpeed, double rightSpeed) {
-    rightDriveTalon.set(rightSpeed);
+    rightDriveTalon.set(rightSpeed);  //setting the speeds of the the drive talon 
     leftDriveTalon.set(leftSpeed);
   }
 
   public void resetEncoders() {
-    leftDriveTalon.setSelectedSensorPosition(0,0,10);
+    leftDriveTalon.setSelectedSensorPosition(0,0,10); //resetting the sensors so it will
     rightDriveTalon.setSelectedSensorPosition(0,0,10);
   }
 
